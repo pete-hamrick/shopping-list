0bff856e2dbec59dfa451549016308b7495bcfb6
Pete Hamrick <pete.hamrick@gmail.com>
2021-12-15 17:41:55 -0800

    ItemList and Item components are functional, no styling yet

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,18 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Item({ item, onEdit, onDelete }) {
+  const [editing, setEditing] = useState(false);
+
+  let itemContent;
+  if (editing) {
+    itemContent = (
+      <>
+        <input
+          value={item.text}
+          onChange={(e) => {
+            onEdit({
+              ...item,
+              text: e.target.value,
+            });
+          }}
+        />
+
+        <button type="button" onClick={() => setEditing(false)}>
+          Save
+        </button>
+      </>
+    );
+  } else {
+    itemContent = (
+      <>
+        <button type="button" onClick={() => setEditing(true)}>
+          Edit
+        </button>
+      </>
+    );
+  }
+
   return (
     <>
-      <label htmlFor={item}>
+      <label
+        htmlFor={item}
+        style={{ textDecoration: item.done ? 'line-through' : null }}
+      >
         <input
           type="checkbox"
           id={item}
-          checked={item.done} /* need onChange for being checked */
+          checked={item.done}
+          onChange={(e) => {
+            onEdit({
+              ...item,
+              done: e.target.checked,
+            });
+          }}
         />
         {item.text}
       </label>
-
-      <button type="button">Edit</button>
+      {itemContent}
       <button type="button" onClick={() => onDelete(item.id)}>
         Delete
       </button>
